a15f49a25b17db8b5a656ec1d2148f1c8a013a22
InnaBedenokEmpeek <inna.bedenok@empeek.com>
2024-10-22 21:25:20 +0300

    third time to commit

--- 7.2.Property_getters_and_setters.js
+++ 7.2.Property_getters_and_setters.js
@@ -8,6 +8,8 @@ let user = {
   };
   
   alert(user.fullName); // John Smith
+//From the outside, an accessor property looks like a regular one. That’s the idea of accessor properties.
+// We don’t call user.fullName as a function, we read it normally: the getter runs behind the scenes.
 
 
-  
+
